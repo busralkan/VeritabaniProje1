90e216ffbbea69458d958ed5e6a4aedc6c629b39
busralkan <alkan.busra@std.izu.edu.tr>
2020-12-20 21:23:24 +0300

    Proje dosyası ekle.

---
+++ GorselProgramlamaProje/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace GorselProgramlamaProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.bttnGirisYap = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblWarning = new System.Windows.Forms.Label();
+            this.cbRemember = new System.Windows.Forms.CheckBox();
+            this.bttnTemizle = new System.Windows.Forms.Button();
+            this.lblWarningMessage = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(483, 212);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(276, 20);
+            this.tbEmail.TabIndex = 0;
+            this.tbEmail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(483, 238);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(276, 20);
+            this.tbPassword.TabIndex = 1;
+            this.tbPassword.UseSystemPasswordChar = true;
+            // 
+            // bttnGirisYap
+            // 
+            this.bttnGirisYap.Location = new System.Drawing.Point(483, 287);
+            this.bttnGirisYap.Name = "bttnGirisYap";
+            this.bttnGirisYap.Size = new System.Drawing.Size(75, 23);
+            this.bttnGirisYap.TabIndex = 2;
+            this.bttnGirisYap.Text = "Giriş Yap";
+            this.bttnGirisYap.UseVisualStyleBackColor = true;
+            this.bttnGirisYap.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(399, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "E-posta Adresi:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(437, 242);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Parola:";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.BackColor = System.Drawing.Color.Transparent;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMessage.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lblMessage.Location = new System.Drawing.Point(326, 141);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(551, 24);
+            this.lblMessage.TabIndex = 5;
+            this.lblMessage.Text = "Bilet satın almak ve daha fazla ayrıntı için lütfen giriş yapın!";
+            this.lblMessage.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.AutoSize = true;
+            this.lblWarning.Location = new System.Drawing.Point(492, 186);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(0, 13);
+            this.lblWarning.TabIndex = 6;
+            // 
+            // cbRemember
+            // 
+            this.cbRemember.AutoSize = true;
+            this.cbRemember.Location = new System.Drawing.Point(483, 264);
+            this.cbRemember.Name = "cbRemember";
+            this.cbRemember.Size = new System.Drawing.Size(80, 17);
+            this.cbRemember.TabIndex = 7;
+            this.cbRemember.Text = "Beni Hatırla";
+            this.cbRemember.UseVisualStyleBackColor = true;
+            // 
+            // bttnTemizle
+            // 
+            this.bttnTemizle.Location = new System.Drawing.Point(577, 287);
+            this.bttnTemizle.Name = "bttnTemizle";
+            this.bttnTemizle.Size = new System.Drawing.Size(75, 23);
+            this.bttnTemizle.TabIndex = 8;
+            this.bttnTemizle.Text = "Temizle";
+            this.bttnTemizle.UseVisualStyleBackColor = true;
+            this.bttnTemizle.Click += new System.EventHandler(this.bttnTemizle_Click);
+            // 
+            // lblWarningMessage
+            // 
+            this.lblWarningMessage.AutoSize = true;
+            this.lblWarningMessage.Location = new System.Drawing.Point(480, 196);
+            this.lblWarningMessage.Name = "lblWarningMessage";
+            this.lblWarningMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblWarningMessage.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(759, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(759, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "*";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(889, 500);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblWarningMessage);
+            this.Controls.Add(this.bttnTemizle);
+            this.Controls.Add(this.cbRemember);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bttnGirisYap);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbEmail);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Giriş";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button bttnGirisYap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblWarning;
+        private System.Windows.Forms.CheckBox cbRemember;
+        private System.Windows.Forms.Button bttnTemizle;
+        private System.Windows.Forms.Label lblWarningMessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ GorselProgramlamaProje/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GorselProgramlamaProje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string email = tbEmail.Text.Trim();
+            string password = tbPassword.Text.Trim();
+
+            lblWarning.Text = "";
+
+            if (email =="")
+            {
+                lblWarning.Text = "Lütfen E-posta adresinizi giriniz.";
+                lblWarning.ForeColor = Color.Red;
+                tbEmail.Focus();
+                return;
+            }
+
+            if (password =="")
+            {
+                lblWarning.Text = "Lütfen parolanızı giriniz.";
+                lblWarning.ForeColor = Color.Red;
+                tbPassword.Focus();
+                return;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttnTemizle_Click(object sender, EventArgs e)
+        {
+            lblWarning.Text = "";
+            tbEmail.Text = "";
+            tbPassword.Text = "";
+            cbRemember.Checked = false;
+        }
+    }
+}
